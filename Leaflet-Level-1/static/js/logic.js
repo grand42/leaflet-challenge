84af0f6914a198982e13863816c91400c5921ef4
Gretchen A <and01487@umn.edu>
2021-01-29 00:55:17 -0600

    fix formatting

--- Leaflet-Level-1/static/js/logic.js
+++ Leaflet-Level-1/static/js/logic.js
@@ -1,24 +1,22 @@
-// Create map object
-var myMap = L.map("map", {
-    center: [40.7, -73.95],
-    zoom: 11,
-    layers:[street, earthquakes]
-  });
-
 // Add tile layer
-var street = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
+var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+  id: "light-v10",
   accessToken: API_KEY
 });
 
-street.addTo(myMap);
-
 // Add earthquake layer
 var earthquakes = L.layerGroup();
+// Create map object
+var myMap = L.map("mapid", {
+    center: [40.7, -73.95],
+    zoom: 11,
+    layers:[lightmap, earthquakes]
+  });
+lightmap.addTo(myMap);
+
+
 
 // Store API
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
@@ -48,12 +46,14 @@ d3.json(url, function(response) {
             return L.circlemarker(latlng ,{
                 color:"black",
                 fillColor: markercolor(feature.geometry.coordinates[2]),
-                radius:markersize(feature.properties.mag)
+                radius:markersize(feature.properties.mag),
+                stroke:True
             });
         }
-    }
+    }).addTo(earthquakes);
  
+earthquakes.addTo(myMap);
+});
+console.log("Test");
 
-});
-});
 
