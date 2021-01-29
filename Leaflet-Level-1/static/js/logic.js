6d28cf1bb36b47821776c4873e64d98ab24d1f96
Gretchen A <and01487@umn.edu>
2021-01-28 21:37:06 -0600

    Add markers

--- Leaflet-Level-1/static/js/logic.js
+++ Leaflet-Level-1/static/js/logic.js
@@ -17,3 +17,28 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 // Store API
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+
+// Grab data with with 
+d3.json(url, function(response) {
+    // Create a new marker cluster group
+  var markers = L.markerClusterGroup();
+  // Loop through data
+  for (var i = 0; i < response.length; i++) {
+
+    // Set the data location property to a variable
+    var location = response[i].location;
+
+    // Check for location property
+    if (location) {
+
+      // Add a new marker to the cluster group and bind a pop-up
+      markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
+        .bindPopup(response[i].descriptor));
+    }
+
+  }
+    // Add our marker cluster layer to the map
+    myMap.addLayer(markers);
+
+});
+
