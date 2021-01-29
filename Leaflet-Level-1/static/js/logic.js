7c1820e7b840335989b347a41afc2fbe62e66979
Gretchen A <and01487@umn.edu>
2021-01-28 23:20:38 -0600

    marker size

--- Leaflet-Level-1/static/js/logic.js
+++ Leaflet-Level-1/static/js/logic.js
@@ -21,8 +21,21 @@ street.addTo(myMap);
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
 // Choose marker color
 
-function markercolor(depth {
+function markercolor(depth) {
+    return depth > 100? '#800026' :
+    depth > 80  ? '#BD0026' :
+    depth  > 60  ? '#E31A1C' :
+    depth  > 40  ? '#FC4E2A' :
+    depth  > 30   ? '#FD8D3C' :
+    depth  > 20   ? '#FEB24C' :
+    depth  > 10   ? '#FED976' :
+               '#FFEDA0';
+    
+}
 
+// marker size
+function markersize(magnitude) {
+    return magnitude * 5
 }
 // Grab data with with 
 d3.json(url, function(response) {
@@ -30,7 +43,9 @@ d3.json(url, function(response) {
     L.geoJson(response, {
         style: function(feature){
             return{
-                color:"white"
+                color:"white",
+                fillColor: markercolor(feature.geometry.coordinates[2]),
+                radius:markersize(feature.properties.mag)
             }
         }
     }
