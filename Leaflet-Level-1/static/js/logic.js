f0a03ae9627172bf7f5cfd4570266db880f9ec96
Gretchen A <and01487@umn.edu>
2021-01-28 23:01:05 -0600

    marker color function

--- Leaflet-Level-1/static/js/logic.js
+++ Leaflet-Level-1/static/js/logic.js
@@ -5,40 +5,36 @@ var myMap = L.map("map", {
   });
 
 // Add tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+var street = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
   tileSize: 512,
   maxZoom: 18,
   zoomOffset: -1,
   id: "mapbox/streets-v11",
   accessToken: API_KEY
-}).addTo(myMap);
+});
+
+street.addTo(myMap);
 
 
 // Store API
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+// Choose marker color
 
+function markercolor(depth {
+
+}
 // Grab data with with 
 d3.json(url, function(response) {
-    // Create a new marker cluster group
-  var markers = L.markerClusterGroup();
-  // Loop through data
-  for (var i = 0; i < response.length; i++) {
-
-    // Set the data location property to a variable
-    var location = response[i].location;
-
-    // Check for location property
-    if (location) {
-
-      // Add a new marker to the cluster group and bind a pop-up
-      markers.addLayer(L.marker([location.coordinates[1], location.coordinates[0]])
-        .bindPopup(response[i].descriptor));
+   
+    L.geoJson(response, {
+        style: function(feature){
+            return{
+                color:"white"
+            }
+        }
     }
-
-  }
-    // Add our marker cluster layer to the map
-    myMap.addLayer(markers);
+ 
 
 });
 
