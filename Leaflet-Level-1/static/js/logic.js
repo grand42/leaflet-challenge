893231e89d46df2a3cf1cce32f88b05913e86853
Gretchen A <and01487@umn.edu>
2021-01-30 21:13:58 -0600

    Fix css to load map

--- Leaflet-Level-1/static/js/logic.js
+++ Leaflet-Level-1/static/js/logic.js
@@ -1,8 +1,10 @@
 // Add tile layer
-var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-  id: "light-v10",
+var lightmap=L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/light-v10",
   accessToken: API_KEY
 });
 
@@ -11,23 +13,22 @@ var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{
 var earthquakes = L.layerGroup();
 // Create map object
 var myMap = L.map("mapid", {
-    center: [40.7, -73.95],
-    zoom: 5,
-    layers:[lightmap, earthquakes]
+    center: [37.09, -95.71],
+    zoom: 2,
   });
+
 lightmap.addTo(myMap);
-
-
 
 // Store API
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
 
 // Grab data with with 
 d3.json(url, function(response) {
-   // Choose marker color
-
+    console.log(response);
+   
+    // Choose marker color
 function markercolor(depth) {
-    return depth > 100? '#800026' :
+    return depth > 100 ? '#800026' :
     depth > 80  ? '#BD0026' :
     depth  > 60  ? '#E31A1C' :
     depth  > 40  ? '#FC4E2A' :
@@ -40,28 +41,32 @@ function markercolor(depth) {
 
 // marker size
 function markersize(magnitude) {
-    return magnitude * 5
+    return magnitude * 4
 }
-function onEachFeature (feature, layer) {
-    layer.bindPopup("<h3>" +feature.properties.place + "<h3><hr><p>Date: " + feature.properties.time + "</p><br><p>Magnitude: "
-    +feature.properties.mag + "</p><br><p>Depth: " + feature.geometry.coordinates[2]+"</p>")
-}
-    L.geoJson(response, {
+
+
+
+L.geoJSON(response, {
         pointtoLayer: function(feature, latlng){
-            return L.circlemarker(latlng ,{
+            return L.circleMarker(latlng, {
                 color:"black",
                 fillColor: markercolor(feature.geometry.coordinates[2]),
                 radius:markersize(feature.properties.mag),
-                stroke:True
+                stroke:True,
+                weight: 1,
+                opacity: 1,
+                fillOpacity: 0.8
             });
-        },
+            },
 
-        onEachFeature: onEachFeature
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup("<h3>" +feature.properties.place + "<h3><hr><p>Date: " + feature.properties.time + "</p><br><p>Magnitude: "
+            +feature.properties.mag + "</p><br><p>Depth: " + feature.geometry.coordinates[2]+"</p>");}
         
-    }).addTo(earthquakes);
+    }).addTo(myMap);
  
-earthquakes.addTo(myMap);
+
 });
-console.log("test");
+
 
 
