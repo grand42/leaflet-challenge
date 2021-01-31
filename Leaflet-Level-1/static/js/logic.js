65fcc42263887012942b16ba8a223b974a8e7b12
Gretchen A <and01487@umn.edu>
2021-01-30 21:56:51 -0600

    Fix color function

--- Leaflet-Level-1/static/js/logic.js
+++ Leaflet-Level-1/static/js/logic.js
@@ -13,8 +13,9 @@ var lightmap=L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y
 var earthquakes = L.layerGroup();
 // Create map object
 var myMap = L.map("mapid", {
-    center: [37.09, -95.71],
+    center: [20, -70],
     zoom: 2,
+    layers:[lightmap, earthquakes]
   });
 
 lightmap.addTo(myMap);
@@ -27,45 +28,52 @@ d3.json(url, function(response) {
     console.log(response);
    
     // Choose marker color
-function markercolor(depth) {
-    return depth > 100 ? '#800026' :
-    depth > 80  ? '#BD0026' :
-    depth  > 60  ? '#E31A1C' :
-    depth  > 40  ? '#FC4E2A' :
-    depth  > 30   ? '#FD8D3C' :
-    depth  > 20   ? '#FEB24C' :
-    depth  > 10   ? '#FED976' :
-               '#FFEDA0';
-    
+function markerColor(depth) {
+    switch(true) {
+        case( depth > 100):
+            return '#FF0000';
+        case( depth > 80):
+            return '#ff8800';
+        case( depth > 50):
+            return '#FFA500 ';
+        case( depth > 20):
+            return '#FFCC00';
+        case( depth > 10):
+            return '#FFFF00';
+        default:
+            return '#99ff00';
+    }
 }
 
 // marker size
-function markersize(magnitude) {
-    return magnitude * 4
+function markerSize(magnitude) {
+    return magnitude * 4;
 }
 
 
 
 L.geoJSON(response, {
-        pointtoLayer: function(feature, latlng){
-            return L.circleMarker(latlng, {
-                color:"black",
-                fillColor: markercolor(feature.geometry.coordinates[2]),
-                radius:markersize(feature.properties.mag),
-                stroke:True,
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.8
-            });
-            },
+    pointToLayer: function (feature, latlng) {
+        return L.circleMarker(latlng, 
+          // Set the style of the markers based on properties.m
+          {
+            radius: markerSize(feature.properties.mag),
+            fillColor: markerColor(feature.geometry.coordinates[2]),
+            fillOpacity: 0.7,
+            color: "black",
+            stroke: true,
+            weight: 0.5
+          }
+        );
+      },
 
         onEachFeature: function (feature, layer) {
             layer.bindPopup("<h3>" +feature.properties.place + "<h3><hr><p>Date: " + new Date(feature.properties.time) + "</p><br><p>Magnitude: "
             +feature.properties.mag + "</p><br><p>Depth: " + feature.geometry.coordinates[2]+"</p>");}
         
-    }).addTo(myMap);
+    }).addTo(earthquakes);
  
-
+earthquakes.addTo(myMap);
 });
 
 
